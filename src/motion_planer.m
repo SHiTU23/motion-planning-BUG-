ed0db8fdea7d74bd86211ce8db7d020a16afedec
SHiTU23 <shi.tu2323@gmail.com>
2024-04-14 19:25:02 +0330

    feat: add another obstacle
    + list env contains all obstacles in environment
    + all intersections with obstacles are found

--- src/motion_planer.m
+++ src/motion_planer.m
@@ -1,4 +1,3 @@
-%%% BUG 2 Algorithm
 clear
 clc
 
@@ -8,9 +7,11 @@ hold on
     
 %% Rect obstacles
 Obst1 = [7, 14 ;8, 14.5; 12, 11.5; 11, 11];
-Obst2 = [17, 20; 18, 20; 22, 15; 21, 15];
-draw_rect(Obst1);
+Obst2 = [17, 20; 18, 20.5; 22, 15.5; 21, 15];
+env = {Obst1, Obst2};
 
+draw_rect(Obst1);
+draw_rect(Obst2);
 
 %% start and target points
 %%%% choose form the plot
@@ -34,38 +35,45 @@ plot(main_path_x, main_path_y, 'k--', 'linewidth', 1);
 
 %% check for intersection with obstacles
 intersection_xy = [];
-for i = 1:4 % 4 lines in obstacles
-    if i == 4
-        next_point = 1;
-    else
-        next_point = i+1;
+
+for obstacle_num = 1:size(env,2)
+    
+    for i = 1:4 % 4 lines in obstacles
+        if i == 4
+            next_point = 1;
+        else
+            next_point = i+1;
+        end
+        env{obstacle_num}(i,:)
+        env{obstacle_num}(next_point,:)
+        new_intersection_xy = intersection_point(start, target, env{obstacle_num}(i,:), env{obstacle_num}(next_point,:));
+        if (new_intersection_xy(1)~=Inf && new_intersection_xy(1)~=-Inf) && (new_intersection_xy(2)~=Inf && new_intersection_xy(2)~=-Inf)
+            intersection_xy = [intersection_xy ; new_intersection_xy, i, next_point, obstacle_num]
+            scatter(new_intersection_xy(1), new_intersection_xy(2), 'bo', 'markerfacecolor', 'green');
+        end
     end
-    new_intersection_xy = intersection_point(start, target, Obst1(i,:), Obst1(next_point,:));
-    if (new_intersection_xy(1)~=Inf && new_intersection_xy(1)~=-Inf) && (new_intersection_xy(2)~=Inf && new_intersection_xy(2)~=-Inf)
-        intersection_xy = [intersection_xy ; new_intersection_xy, i, next_point];
-        scatter(new_intersection_xy(1), new_intersection_xy(2), 'bo', 'markerfacecolor', 'green');
-    end
+    i =1;
 end
 closest_intersections = order_closer_points(start, intersection_xy);
-
-%% Go towards Target point
-% Go from start point to first intersection %% FIRST OBSTACLE
-current_pose = start;
-% for points = 1:size(closest_intersections,1)
-     plot_line(current_pose, closest_intersections(1,1:2));
-     current_pose = closest_intersections(1,1:2);
-     nearest_point = find_nearest_point(current_pose, Obst1(closest_intersections(1,3),:), Obst1(closest_intersections(1,4),:));
-     plot_line(current_pose, nearest_point);
-     current_pose = nearest_point;
-     
-     Obst1(closest_intersections(1,3),:) = [];
-     Obst1(closest_intersections(1,4),:) = [];
-     next_point_togo = find_nearest_point(nearest_point, Obst1(1,:), Obst1(2,:));
-     plot_line(current_pose, next_point_togo);
-     current_pose = next_point_togo;
-     plot_line(current_pose, closest_intersections(2,1:2));
-     current_pose = closest_intersections(2,1:2);
-% end
-
-%% plot to the target
-plot_line(current_pose, target);
+% 
+% %% Go towards Target point
+% % Go from start point to first intersection %% FIRST OBSTACLE
+% current_pose = start;
+% % for points = 1:size(closest_intersections,1)
+%      plot_line(current_pose, closest_intersections(1,1:2));
+%      current_pose = closest_intersections(1,1:2);
+%      nearest_point = find_nearest_point(current_pose, Obst1(closest_intersections(1,3),:), Obst1(closest_intersections(1,4),:));
+%      plot_line(current_pose, nearest_point);
+%      current_pose = nearest_point;
+%      
+%      Obst1(closest_intersections(1,3),:) = [];
+%      Obst1(closest_intersections(1,4),:) = [];
+%      next_point_togo = find_nearest_point(nearest_point, Obst1(1,:), Obst1(2,:));
+%      plot_line(current_pose, next_point_togo);
+%      current_pose = next_point_togo;
+%      plot_line(current_pose, closest_intersections(2,1:2));
+%      current_pose = closest_intersections(2,1:2);
+% % end
+% 
+% %% plot to the target
+% plot_line(current_pose, target);
