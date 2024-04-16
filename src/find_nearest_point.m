292d28d67a1e80c0ef2e56f7edda9887001e44f5
SHiTU23 <shi.tu2323@gmail.com>
2024-04-16 14:44:02 +0330

    feat: find the closest point to the first input
    + takes two points to find the closest one to the current pose
    + gives the x,y values and the index of the closest point

---
+++ src/find_nearest_point.m
@@ -0,0 +1,10 @@
+function nearest_point = find_nearest_point(current_pose, point1, point2)
+    L1 = sqrt((current_pose(1)-point1(1))^2 + (current_pose(2)-point1(2))^2);
+    L2 = sqrt((current_pose(1)-point2(1))^2 + (current_pose(2)-point2(2))^2);
+    if L1 <= L2
+        nearest_point = [point1, 1];
+    else
+        nearest_point = [point2, 2];
+    end
+end
+
