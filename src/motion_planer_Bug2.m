17a31b10d7c296498e39716650db82b6215aa554
SHiTU23 <shi.tu2323@gmail.com>
2024-04-17 18:57:12 +0330

    Fix: if obstacle has only one intersection with the start-target line

--- src/motion_planer_Bug2.m
+++ src/motion_planer_Bug2.m
@@ -8,7 +8,7 @@ hold on
 %%%%% BUG 2 FOR MOTION PLANNING %%%%%
 %% Rect obstacles
 Obst1 = [7, 14 ;8, 14.5; 12, 11.5; 11, 11];
-Obst3 = rect_generator([15, 15.5], -90);
+Obst3 = rect_generator([15, 15], -90);
 Obst2 = [17, 20; 18, 20.5; 22, 15.5; 21, 15];
 env = {Obst1, Obst2, Obst3};
 env_copy = env;
@@ -66,52 +66,69 @@ closest_intersections = order_closer_points(start, intersection_xy);
 % Go from start point to first intersection %% FIRST OBSTACLE
 current_pose = start;
 obstacle_id = 1;
-     
+object_with_one_intersection = 0;
+number_of_OI_objects = 0;
 for intersections = 1:2:size(closest_intersections,1)
     %%% first intersection
-     obstacle_id = closest_intersections(intersections,5);
-     plot_line(current_pose, closest_intersections(intersections,1:2));
-     current_pose = closest_intersections(intersections,1:2);
-     
-     %%% nearest corner
-     nearest_point = find_nearest_point(current_pose, env{obstacle_id}(closest_intersections(intersections,3),:), env{obstacle_id}(closest_intersections(intersections,4),:));
-     plot_line(current_pose, nearest_point(1,1:2));
-     current_pose = nearest_point(1,1:2);
-    
-     %%% find the other two corners
-     %%% delete the two points from first line we went through
-     env_copy{obstacle_id}(closest_intersections(intersections,3),:) = 0;
-     env_copy{obstacle_id}(closest_intersections(intersections,4),:) = 0;
-     for i = 1:4
-         if env_copy{obstacle_id}(i,1) ~= 0 && env_copy{obstacle_id}(i,2) ~=0
-             third_point = i;
-             env_copy{obstacle_id}(i,:) = 0;
-             break
-         end
-     end
-     for i = 1:4
-         if env_copy{obstacle_id}(i,1) ~= 0 && env_copy{obstacle_id}(i,2) ~=0
-             forth_point = i;
-             env_copy{obstacle_id}(i,:) = 0;
-         end
-     end
-     %%% find the next closest corner
-     next_point_togo = find_nearest_point(current_pose, env{obstacle_id}(third_point,:), env{obstacle_id}(forth_point,:));
-     next_intersection = closest_intersections(intersections+1,1:2)
-     %%% check if intersection is on this side
-     intersection_onThisSide = is_onTheLine([current_pose;next_point_togo(1,:)], next_intersection);
-     if intersection_onThisSide == 0 %%% intersection is not in this line, go to the next corner
-         %%% third corner to go
-         plot_line(current_pose, next_point_togo(1,1:2));
-         current_pose = next_point_togo(1,1:2);
-         
-         %%% next intersection
-         plot_line(current_pose, next_intersection);
-         current_pose = next_intersection;
+     if object_with_one_intersection == 0 %%% the object had 2 intersections
+         intersections_ID = intersections;
      else
-         %%% next intersection
-         plot_line(current_pose, next_intersection);
-         current_pose = next_intersection;
+         intersections_ID = intersections-number_of_OI_objects;
+     end
+     
+     obstacle_id = closest_intersections(intersections_ID,5);
+     plot_line(current_pose, closest_intersections(intersections_ID,1:2));
+     current_pose = closest_intersections(intersections_ID,1:2);
+     
+     %%% if there is only one intersection with an obstacle, it means it is
+     %%% in the corner, so go to the next iteration
+     next_intersection_objectID = closest_intersections(intersections_ID+1,5);
+     if next_intersection_objectID == obstacle_id
+     
+         %%% nearest corner
+         nearest_point = find_nearest_point(current_pose, env{obstacle_id}(closest_intersections(intersections_ID,3),:), env{obstacle_id}(closest_intersections(intersections_ID,4),:));
+         plot_line(current_pose, nearest_point(1,1:2));
+         current_pose = nearest_point(1,1:2);
+
+
+         %%% find the other two corners
+         %%% delete the two points from first line we went through
+         env_copy{obstacle_id}(closest_intersections(intersections_ID,3),:) = 0;
+         env_copy{obstacle_id}(closest_intersections(intersections_ID,4),:) = 0;
+         for i = 1:4
+             if env_copy{obstacle_id}(i,1) ~= 0 && env_copy{obstacle_id}(i,2) ~=0
+                 third_point = i;
+                 env_copy{obstacle_id}(i,:) = 0;
+                 break
+             end
+         end
+         for i = 1:4
+             if env_copy{obstacle_id}(i,1) ~= 0 && env_copy{obstacle_id}(i,2) ~=0
+                 forth_point = i;
+                 env_copy{obstacle_id}(i,:) = 0;
+             end
+         end
+         %%% find the next closest corner
+         next_point_togo = find_nearest_point(current_pose, env{obstacle_id}(third_point,:), env{obstacle_id}(forth_point,:));
+         next_intersection = closest_intersections(intersections_ID+1,1:2);
+         %%% check if intersection is on this side
+         intersection_onThisSide = is_onTheLine([current_pose;next_point_togo(1,:)], next_intersection);
+         if intersection_onThisSide == 0 %%% intersection is not in this line, go to the next corner
+             %%% third corner to go
+             plot_line(current_pose, next_point_togo(1,1:2));
+             current_pose = next_point_togo(1,1:2);
+
+             %%% next intersection
+             plot_line(current_pose, next_intersection);
+             current_pose = next_intersection;
+         else
+             %%% next intersection
+             plot_line(current_pose, next_intersection);
+             current_pose = next_intersection;
+         end
+     else
+         object_with_one_intersection = 1;
+         number_of_OI_objects = number_of_OI_objects + 1;
      end
 end
 
